b210b752374b9863cc9e08c272e4c43481d20fce
achucarrob <achucarroclaudia@gmail.com>
2023-08-15 16:34:02 -0400

    modificacion del guion 3

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hola, mundo!"
+def index():
+    return "Hello, World!"
 
-@app.route("/hapi")
+@app.route("/saludo")
+def saludo():
+    return render_template("index.html")
+
+@app.route("/tarjeta")
 def tarjeta():
-    return render_template ('tarjeta.html')
+    nombre = 'Pablo'
+    especie = 'Backyardigan'
+    return render_template ('tarjeta_python.html', nombre = nombre , especie = especie)
+
+@app.route("/personaje")
+def personaje():
+    # Definimos la url a la que se hace el pedido 
+    url = 'https://rickandmortyapi.com/api/character/4'
+    
+    # Hacemos el pedido a la API -- nos devuelve un diccionario
+    respuesta_api = requests.get(url).json()
+
+    # Creamos variables a traves de acceder la respuesta
+    nombre = respuesta_api['name']
+    estado = respuesta_api["status"]
+    imagen = respuesta_api['image']
+
+    return render_template('personaje.html', nombre=nombre, estado=estado, imagen=imagen)
+
+@app.route("/personajes")
+def todos_los_personajes():
+
+    url = 'https://rickandmortyapi.com/api/character'
+
+    respuesta = requests.get(url).json()
+
+    lista_de_personajes = respuesta ['results']
+
+    return render_template('rick_and_morty.html', lista_de_personajes = lista_de_personajes)
